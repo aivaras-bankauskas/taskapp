9d646f0bceaee4fef6e762739f1c7f3a9043ff6e
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 22:13:50 +0300

    TaskCard: add badge color function

--- src/views/components/TaskCard.vue
+++ src/views/components/TaskCard.vue
@@ -12,12 +12,20 @@ const task = defineProps({
     default: false
   }
 })
+const badgeColor = (status) => {
+  if (status === 'Done') return 'success'
+  if (status === 'In progress') return 'warning'
+  if (status === 'Closed') return 'danger'
+  return 'info'
+}
 </script>
 <template>
   <div class="card">
     <div class="card-body pb-3">
       <h5 class="card-title">{{ task.title }}</h5>
-      <span class="badge rounded-pill fw-normal my-2 px-3 text-bg-info">{{ task.status }}</span>
+      <span :class="`badge rounded-pill fw-normal my-2 px-3 text-bg-${badgeColor(task.status)}`">
+        {{ task.status }}
+      </span>
       <p class="card-description card-text">{{ task.description }}</p>
       <ButtonGroup :show-buttons="task.showButtons" :id="task.id" />
     </div>
