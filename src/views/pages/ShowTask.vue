a3cfc3289eea022029af7af5de9e0024365b88d6
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 21:13:17 +0300

    ShowTask: update TaskCard props

--- src/views/pages/ShowTask.vue
+++ src/views/pages/ShowTask.vue
@@ -12,7 +12,13 @@ const task = data.tasks.find((task) => task.id === Number(props.id))
   <div v-if="task" class="my-2">
     <div class="row">
       <div class="col-12 col-lg-9">
-        <TaskCard :title="task.title" :description="task.description" :status="task.status" />
+        <TaskCard
+          :title="task.title"
+          :description="task.description"
+          :status="task.status"
+          :show-buttons="true"
+          :id="Number(props.id)"
+        />
       </div>
       <div class="col-12 col-lg-3">
         <SidebarLayout />
