fee2630947a58bc01d2f95cf81cb7ad6e283ca11
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:28:43 +0300

    ShowTask: create component

---
+++ src/views/pages/ShowTask.vue
@@ -0,0 +1,11 @@
+<script setup>
+import data from '../../database/data.json'
+import TaskCard from '../components/TaskCard.vue'
+
+const props = defineProps({ id: String })
+
+const task = data.tasks.find((task) => task.id === Number(props.id))
+</script>
+<template>
+  <TaskCard :title="task.title" :description="task.description" :status="task.status" />
+</template>
