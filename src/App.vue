2965e5d75365ff9adb24dc14e9df2f3367b3eea7
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:00:58 +0300

    App: import data from data.json

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import data from './database/data.json'
 import TaskCard from './views/components/TaskCard.vue'
 import NavbarLayout from './views/layout/NavbarLayout.vue'
 </script>
@@ -8,14 +9,8 @@ import NavbarLayout from './views/layout/NavbarLayout.vue'
   <div class="container">
     <div class="my-2">
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 p-1">
-        <div class="col p-2">
-          <TaskCard title="First Task" description="Lorem Ipsum" status="Opened" />
-        </div>
-        <div class="col p-2">
-          <TaskCard title="Second Task" description="Lorem Ipsum" status="In Progress" />
-        </div>
-        <div class="col p-2">
-          <TaskCard title="Third Task" description="Lorem Ipsum" status="Done" />
+        <div class="col p-2" v-for="task in data.tasks" :key="task.id">
+          <TaskCard :title="task.title" :description="task.description" :status="task.status" />
         </div>
       </div>
     </div>
