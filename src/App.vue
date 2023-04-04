151268ac8de80ea62d1a215d84730fe82999595e
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:11:45 +0300

    App: add router view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
+import { RouterView } from 'vue-router'
 import NavbarLayout from './views/layout/NavbarLayout.vue'
-import AllTasks from './views/pages/AllTasks.vue'
 </script>
 
 <template>
   <NavbarLayout />
   <div class="container">
-    <AllTasks />
+    <RouterView />
   </div>
 </template>
