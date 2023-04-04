90d34922d6d95d8a3573f546a0881de7f0bc40ee
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 21:26:51 +0300

    ButtonGroup: create component

---
+++ src/views/components/ButtonGroup.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  id: Number,
+  showButtons: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+<template>
+  <div class="text-end" v-if="props.showButtons">
+    <button class="btn btn-link btn-sm text-danger mx-1">
+      <i class="bi bi-x-octagon"></i> Delete task
+    </button>
+    <RouterLink
+      class="btn btn-link btn-sm text-warning mx-1"
+      :to="{ name: 'editTask', params: { id: props.id } }"
+    >
+      <i class="bi bi-pencil-square"></i> Edit task
+    </RouterLink>
+    <RouterLink class="btn btn-link btn-sm text-primary mx-1" :to="{ name: 'allTasks' }">
+      <i class="bi bi-x-lg"></i> Close
+    </RouterLink>
+  </div>
+</template>
