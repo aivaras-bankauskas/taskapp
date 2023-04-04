0fd9968a224120b9f872184c5112d83e6bb4e147
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:46:51 +0300

    ShowTask: add sidebarLayout

--- src/views/pages/ShowTask.vue
+++ src/views/pages/ShowTask.vue
@@ -1,11 +1,21 @@
 <script setup>
 import data from '../../database/data.json'
 import TaskCard from '../components/TaskCard.vue'
+import SidebarLayout from '../layout/SidebarLayout.vue'
 
 const props = defineProps({ id: String })
 
 const task = data.tasks.find((task) => task.id === Number(props.id))
 </script>
 <template>
-  <TaskCard :title="task.title" :description="task.description" :status="task.status" />
+  <div v-if="task" class="my-2">
+    <div class="row">
+      <div class="col-12 col-lg-9">
+        <TaskCard :title="task.title" :description="task.description" :status="task.status" />
+      </div>
+      <div class="col-12 col-lg-3">
+        <SidebarLayout />
+      </div>
+    </div>
+  </div>
 </template>
