098cc25c0ca945e275e327145a625f36619051ec
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 20:29:11 +0300

    Createtask: create and import BaseForm component

--- src/views/pages/CreateTask.vue
+++ src/views/pages/CreateTask.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import BaseForm from '../components/base/BaseForm.vue'
 </script>
 <template>
   <div class="card m-auto" style="max-width: 24rem">
@@ -10,39 +11,7 @@ import { RouterLink } from 'vue-router'
           <i class="bi bi-x-lg"></i>
         </RouterLink>
       </div>
-      <form>
-        <div class="mb-3 mt-3">
-          <label for="task-title" class="form-label">Title</label>
-          <input
-            type="text"
-            id="task-title"
-            class="form-control"
-            placeholder="First Task"
-            maxlength="200"
-            minlength="5"
-            required
-          />
-        </div>
-        <div class="mb-3 mt-3">
-          <select class="form-select">
-            <option selected>Opened</option>
-            <option value="In progress">In progress</option>
-            <option value="Done">Done</option>
-            <option value="Closed">Closed</option>
-          </select>
-        </div>
-        <div class="mb-3">
-          <label for="task-description" class="form-label">Description</label>
-          <textarea
-            id="task-description"
-            class="form-control"
-            rows="5"
-            maxlength="2000"
-            required
-          ></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Create task</button>
-      </form>
+      <BaseForm />
     </div>
   </div>
 </template>
