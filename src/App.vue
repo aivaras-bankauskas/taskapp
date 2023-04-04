3ddf0cc3ec6c8f6c883b8039df5090c501f75d1a
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 18:36:11 +0300

    App: add navbar and base layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,52 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <nav class="navbar">
+    <div class="container">
+      <a class="navbar-brand text-white fs-3" href="/">Task Manager</a>
+      <a class="btn btn-primary px-3" href="/tasks/create">Add task</a>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="my-2">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 p-1">
+        <div class="col p-2">
+          <div class="card">
+            <div class="card-body pb-5">
+              <h5 class="card-title">First Task</h5>
+              <span class="badge rounded-pill fw-normal my-2 px-3 text-bg-info"> Opened </span>
+              <p class="card-description card-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem minima,
+                sequi autem voluptates consequatur iusto! Possimus neque officiis unde rem.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col p-2">
+          <div class="card">
+            <div class="card-body pb-5">
+              <h5 class="card-title">First Task</h5>
+              <span class="badge rounded-pill fw-normal my-2 px-3 text-bg-info"> Opened </span>
+              <p class="card-description card-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem minima,
+                sequi autem voluptates consequatur iusto! Possimus neque officiis unde rem.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col p-2">
+          <div class="card">
+            <div class="card-body pb-5">
+              <h5 class="card-title">First Task</h5>
+              <span class="badge rounded-pill fw-normal my-2 px-3 text-bg-info"> Opened </span>
+              <p class="card-description card-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem minima,
+                sequi autem voluptates consequatur iusto! Possimus neque officiis unde rem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
