1d3ac4b53745b089b53cce7752717e1004369d29
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:29:18 +0300

    index.js: add show task route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AllTasks from '../views/pages/AllTasks.vue'
+import ShowTask from '../views/pages/ShowTask.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'allTask',
       component: AllTasks
+    },
+    {
+      path: '/tasks/:id',
+      props: true,
+      name: 'showTask',
+      component: ShowTask
     }
   ]
 })
