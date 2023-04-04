06878306f8af153012bb265cbaccac9eaaceb56e
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:06:42 +0300

    App: create and import AllTasks component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-import data from './database/data.json'
-import TaskCard from './views/components/TaskCard.vue'
 import NavbarLayout from './views/layout/NavbarLayout.vue'
+import AllTasks from './views/pages/AllTasks.vue'
 </script>
 
 <template>
   <NavbarLayout />
   <div class="container">
-    <div class="my-2">
-      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 p-1">
-        <div class="col p-2" v-for="task in data.tasks" :key="task.id">
-          <TaskCard :title="task.title" :description="task.description" :status="task.status" />
-        </div>
-      </div>
-    </div>
+    <AllTasks />
   </div>
 </template>
---
+++ src/views/pages/AllTasks.vue
@@ -0,0 +1,13 @@
+<script setup>
+import data from '../../database/data.json'
+import TaskCard from '../../views/components/TaskCard.vue'
+</script>
+<template>
+  <div class="my-2">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 p-1">
+      <div class="col p-2" v-for="task in data.tasks" :key="task.id">
+        <TaskCard :title="task.title" :description="task.description" :status="task.status" />
+      </div>
+    </div>
+  </div>
+</template>
