dd917b54e507c26a0a94d99b57dafa4698079ca3
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:33:02 +0300

    NavbarLayout: add router link

--- src/views/layout/NavbarLayout.vue
+++ src/views/layout/NavbarLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="navbar">
     <div class="container">
-      <a class="navbar-brand text-white fs-3" href="/">Task Manager</a>
-      <a class="btn btn-primary px-3" href="/tasks/create">Add task</a>
+      <RouterLink class="navbar-brand text-white fs-3" to="/">Task Manager</RouterLink>
+      <RouterLink class="btn btn-primary px-3" to="/tasks/create">Add task</RouterLink>
     </div>
   </nav>
 </template>
