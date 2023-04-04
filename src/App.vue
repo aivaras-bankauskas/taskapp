d8d10c05e31b551068bff2255932cec25853686c
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 18:49:39 +0300

    TaskCard: add props

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,13 @@ import NavbarLayout from './views/layout/NavbarLayout.vue'
     <div class="my-2">
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 p-1">
         <div class="col p-2">
-          <TaskCard />
+          <TaskCard title="First Task" description="Lorem Ipsum" status="Opened" />
+        </div>
+        <div class="col p-2">
+          <TaskCard title="Second Task" description="Lorem Ipsum" status="In Progress" />
+        </div>
+        <div class="col p-2">
+          <TaskCard title="Third Task" description="Lorem Ipsum" status="Done" />
         </div>
       </div>
     </div>
--- src/views/components/TaskCard.vue
+++ src/views/components/TaskCard.vue
@@ -1,12 +1,17 @@
+<script setup>
+const task = defineProps({
+  id: Number,
+  title: String,
+  description: String,
+  status: String
+})
+</script>
 <template>
   <div class="card">
     <div class="card-body pb-5">
-      <h5 class="card-title">First Task</h5>
-      <span class="badge rounded-pill fw-normal my-2 px-3 text-bg-info"> Opened </span>
-      <p class="card-description card-text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem minima, sequi autem
-        voluptates consequatur iusto! Possimus neque officiis unde rem.
-      </p>
+      <h5 class="card-title">{{ task.title }}</h5>
+      <span class="badge rounded-pill fw-normal my-2 px-3 text-bg-info">{{ task.status }}</span>
+      <p class="card-description card-text">{{ task.description }}</p>
     </div>
   </div>
 </template>
