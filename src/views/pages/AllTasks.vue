7903e6de9a85ae6f248d1bd417012c4badea5769
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 22:13:03 +0300

    AllTasks: add get all tasks function

--- src/views/pages/AllTasks.vue
+++ src/views/pages/AllTasks.vue
@@ -1,12 +1,28 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import { RouterLink } from 'vue-router'
-import data from '../../database/data.json'
+import TaskService from '../../services/TaskService'
 import TaskCard from '../../views/components/TaskCard.vue'
+
+const tasks = ref([])
+
+onMounted(() => {
+  getAllTasks()
+})
+
+const getAllTasks = async () => {
+  try {
+    const response = await TaskService.getTasks()
+    tasks.value.push(...response.data)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 </script>
 <template>
   <div class="my-2">
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 p-1">
-      <div class="col p-2" v-for="task in data.tasks" :key="task.id">
+      <div class="col p-2" v-for="task in tasks" :key="task.id">
         <RouterLink
           class="text-decoration-none text-black"
           :to="{ name: 'showTask', params: { id: task.id } }"
