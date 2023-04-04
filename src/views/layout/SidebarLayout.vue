af4d313e20986f2ad4d78d892a968054dd4b5bfd
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:54:16 +0300

    SidebarLayout: create component add BaseTabRow

---
+++ src/views/layout/SidebarLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BaseTableRow from '../components/base/BaseTableRow.vue'
+</script>
+<template>
+  <div class="card bg-light w-100 h-100 my-4 my-lg-0">
+    <div class="card-body">
+      <table class="table table-borderless table-sm align-middle">
+        <tbody>
+          <BaseTableRow title="Project" option="Task manager" color="text-primary" />
+          <BaseTableRow title="Priority" option="Normal" color="text-info" />
+          <BaseTableRow title="Type" option="bug" color="text-danger" />
+          <BaseTableRow title="Assignee" option="Unnasigned" color="text-primary" />
+          <BaseTableRow title="Release Date" option="22 Feb 2023" color="text-primary" />
+          <BaseTableRow title="Estimations" option="5h" color="text-info" />
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
