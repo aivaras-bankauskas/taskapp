864572f76e7e515389a51599d0da05aaccf5a334
aivaras <aivarasbankauskas@gmail.com>
2023-04-04 19:12:44 +0300

    index.js: add all tasks route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AllTasks from '../views/pages/AllTasks.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'allTask',
+      component: AllTasks
+    }
+  ]
 })
 
 export default router
